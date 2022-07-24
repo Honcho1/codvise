5166008b22b50ec65714823fc8bfb28fe8a52274
Daniel Nwadinkpa <kingdanofficial@gmail.com>
2022-07-24 07:42:57 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 var myName = "Daniel";
 var yourName = prompt("What's your name?");
-alert("My name is " + myName + ", welcome to Codvise Consulting Firm " + yourName +"!"); 
+alert("My name is " + myName + ", welcome to Codvise Consulting Firm " + yourName + "!"); 
